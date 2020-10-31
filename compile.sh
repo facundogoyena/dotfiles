2c59ebffd68ecca39db521b3e96d4fee28377c36
Facundo Goyena <facundo.goyena@gmail.com>
2020-10-31 19:22:39 -0300

    Added my examples for .env. Added trap for compile. Compile now also replaces a wildcard for win home so it's more reusable.

--- compile.sh
+++ compile.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-# TODO: merge (probably won't do it)
+# trap exit to avoid keeping the user in another
+# directory if script fails or gets cancelled
+ORIGINAL_DIR=$(pwd)
+
+function exit_trap {
+    cd $ORIGINAL_DIR
+}
+
+set -e
+trap exit_trap EXIT
 
 display_usage() {
     echo "This script will compile all config files that needs to be combined for OS specific settings."
@@ -18,6 +27,15 @@ then
     exit 0
 fi
 
+# switch to this script's directory
+SCRIPT_DIR="$(cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
+cd $SCRIPT_DIR
+
+# get .env vars
+if [ -f ".env" ]; then
+    source .env
+fi
+
 # sh options
 shopt -s globstar
 shopt -s nullglob
@@ -33,9 +51,13 @@ for file in **/*-{win,unix,osx}; do
 
     if [ ! -f $basefile ]; then
         cat $file > $outputfile
-        continue
+    else
+        cat $basefile $file > $outputfile
     fi
 
-    cat $basefile $file > $outputfile
+    sed -i "s/\#REPLACE_WIN_HOME\#/$WIN_HOME_REPLACE/" $outputfile
 done
+
+# go back to original dir
+cd $ORIGINAL_DIR
 
