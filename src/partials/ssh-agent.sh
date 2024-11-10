73cddab7f097fbfb6e2e9461a3eb82bb3538819a
Facundo Goyena <facundo.goyena@gmail.com>
2024-11-10 20:33:03 +0000

    Update ssh-agent script.

--- src/partials/ssh-agent.sh
+++ src/partials/ssh-agent.sh
@@ -3,7 +3,7 @@ SSH_ENV=$HOME/.ssh/environment
 
 function start_agent {
     # kill all existing ssh-agent
-    ps | grep -i ssh-agent | awk '{print $1}' | while read pid; do kill ${pid}; done; 2>&1 > /dev/null
+    ps | grep -i ssh-agent | awk '{print $1}' | while read pid; do kill ${pid}; done;
 
     # spawn ssh-agent
     /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
